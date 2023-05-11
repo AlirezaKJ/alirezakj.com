112e3fcbea1bad090c753c13d6ad2488c0f159cd
AlirezaKJ <akarimijafari@gmail.com>
2023-05-11 20:16:29 +1000

    scrolldown arrow functionality

--- bsc/lib/js/main.js
+++ bsc/lib/js/main.js
@@ -17,3 +17,9 @@ function xilidirect(url) {
         window.location.href = url
     }, 500);
 }
+
+function scrolldowntosection() {
+    var firstsection = document.querySelector(".fixbg").getBoundingClientRect();
+    window.scrollTo(0, firstsection.top);
+    console.log("scroll req received")
+}
