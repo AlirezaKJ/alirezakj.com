b5dcf6324760b1c72ba7bbde1973c5af443a8390
AlirezaKJ <akarimijafari@gmail.com>
2025-03-31 23:05:35 +1100

    implementation by far

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -1,9 +1,11 @@
 import { Link, Outlet } from "react-router"
+import './css/nav.css'
 
 const Nav = () => {
   return (
     <>
       <nav>
+        <div className="letterbar"></div>
         <Link to="/">Home</Link>
         <Link to="/BSC">BSC</Link>
         <Link to="https://ptable.alirezakj.com/">PTable</Link>
