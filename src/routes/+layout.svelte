b3be944e88bd100d8fddb74b4c578b70f6a313b0
AlirezaKJ <akarimijafari@gmail.com>
2025-08-29 15:15:24 +1000

    added layout bones

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,19 @@
 <script>
-	import '../app.css';
+	import '$lib/main.css';
 	import favicon from '$lib/assets/favicon.svg';
 
-	let { children } = $props();
+	import Footer from '$lib/Components/Footer.svelte';
+	import Nav from '$lib/Components/Nav.svelte';
+
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<title> HOME | AKJ </title>
 </svelte:head>
 
-{@render children?.()}
+<Nav />
+
+<slot />
+
+<Footer />
