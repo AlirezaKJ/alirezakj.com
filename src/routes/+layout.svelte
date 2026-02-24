14e4da96a53007e7b4c358aa7382d72c94b66387
AlirezaKJ <akarimijafari@gmail.com>
2026-02-24 13:09:23 +1100

    added the line to initialize vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,13 +4,14 @@
 	import favicon from '$lib/assets/favicon.svg';
 	import Footer from '$lib/Components/Layouts/Footer.svelte';
 	import Nav from '$lib/Components/Layouts/Nav.svelte';
-
 	import { onMount } from 'svelte';
 	import Lenis from 'lenis';
 	import 'lenis/dist/lenis.css';
 	import { gsap } from 'gsap';
 	import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 	import { scrollState, lenisStore } from '$lib/stores/scroll';
+	
+	injectAnalytics();
 
 	onMount(() => {
 		gsap.registerPlugin(ScrollTrigger);
