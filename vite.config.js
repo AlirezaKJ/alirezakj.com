f0960e9e3936ef7491eabc995790216a4defebe4
AlirezaKJ <akarimijafari@gmail.com>
2026-09-10 14:22:50 +1000

    changed adapter to cloudflare

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import tailwindcss from '@tailwindcss/vite';
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
